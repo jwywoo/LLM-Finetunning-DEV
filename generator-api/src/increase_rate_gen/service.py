77d64937df3cd848763cae4ce653785423fd20c9
Wooyong Jeong(Woo) <60910348+jwywoo@users.noreply.github.com>
2025-05-11 19:19:42 +0900

    [FEAT] #7-2 FastAPI Model Loader and Generator Set

--- generator-api/src/increase_rate_gen/service.py
+++ generator-api/src/increase_rate_gen/service.py
@@ -1,5 +1,17 @@
 from .schema import IncreaseRateGenRequestDto
 
+from .method.generator import increase_rate_generator
+from .method.model_loader import load_model
+
 
 def increase_rate_gen_service(request: IncreaseRateGenRequestDto):
+    prompt = ""
+    tokenizer, model = load_model()
+
+    result = increase_rate_generator(
+        model=model,
+        tokenizer=tokenizer,
+        prompt=prompt,
+        max_new_tokens=280
+    )
     return "Testing"
